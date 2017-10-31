6a421146338ce74b3e96a2fc128e053d4cda0d6f
Andrey <Andrey@192.168.0.101>
2017-10-30 22:30:19 -0200

    desenvolvimento sistema para se conectar ao banco de dados

---
+++ tadsstore-common/src/main/resources/META-INF/02-SCRIPT-DADOS.sql
@@ -0,0 +1,23 @@
+INSERT INTO TB_CATEGORIA (DS_CATEGORIA) VALUES
+('Memória RAM'),
+('Processador'),
+('Placa mãe'),
+('Placa de vídeo'),
+('Gabinete'),
+('Jogos'),
+('Fonte para Desktop'),
+('Notebook'),
+('Teclado'),
+('Mouse');
+
+INSERT INTO TB_PRODUTO (NM_PRODUTO, DS_PRODUTO, DT_CADASTRO, VL_PRODUTO) VALUES
+('Placa de vídeo GTX 1080ti', '', CURRENT_TIMESTAMP, 2199.90),
+('Processador intel i7 7700k', '', CURRENT_TIMESTAMP, 1390.90);
+
+INSERT INTO TB_IMAGENS (DS_NOME, DS_IMAGEM, ID_PRODUTO) VALUES 
+('picture01.jpg', 'Imagem Placa de vídeo GTX 1080ti', 1),
+('picture02.jpg', 'Processador intel i7 7700k', 2);
+
+INSERT INTO TB_PRODUTO_CATEGORIA (ID_PRODUTO, ID_CATEGORIA) VALUES 
+(1, 1),
+(2, 2);
