25b40992ddf2becc564c9970765aace8cd423228
andrey.santos <andrey.santos@NBAndrey.password.local>
2017-11-07 12:55:44 -0200

    Correção de problemas com venda.

--- tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/controller/GerenciaVenda.java
+++ tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/controller/GerenciaVenda.java
@@ -1,6 +1,7 @@
 package br.senac.tads4.dsw.tadsstore.controller;
 
 import br.senac.tads4.dsw.tadsstore.common.entity.Venda;
+import br.senac.tads4.dsw.tadsstore.common.repository.VendaServiceJPAImpl;
 import br.senac.tads4.dsw.tadsstore.common.service.VendaService;
 import java.util.Date;
 import java.util.LinkedHashSet;
@@ -20,48 +21,47 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/gerenciamento/venda")
 public class GerenciaVenda {
 
-  @Autowired
-  private VendaService vendaService;
+    private VendaService vendaService = new VendaServiceJPAImpl();
 
-  @RequestMapping
-  public ModelAndView abrirFormulario() {
-    return new ModelAndView("venda/input")
-            .addObject("venda", new Venda());
-  }
-
-  @RequestMapping("/{id}")
-  public ModelAndView abrirAlteracao(@PathVariable("id") Long idVenda) {
-    Venda v = vendaService.obter(idVenda);
-    return new ModelAndView("venda/input")
-            .addObject("venda", v);
-  }
-
-  @RequestMapping(value = "/{id}/remover", method = RequestMethod.POST)
-  public ModelAndView removerProduto(@PathVariable("id") Long idVenda) {
-    vendaService.remover(idVenda);
-    return new ModelAndView("redirect:/gerenciamento/venda");
-  }
-
-  @RequestMapping(value = "/salvar", method = RequestMethod.POST)
-  public ModelAndView salvar(
-          @ModelAttribute("venda") @Valid Venda v,
-          BindingResult bindingResult,
-          RedirectAttributes redirectAttributes) {
-    if (bindingResult.hasErrors()) {
-      return new ModelAndView("venda/input");
-    }
-    boolean inclusao = (v.getId() == null);
-    v.setDtVenda(new Date());
-
-    if (inclusao) {
-      vendaService.incluir(v);
-    } else {
-      vendaService.alterar(v);
+    @RequestMapping
+    public ModelAndView abrirFormulario() {
+        return new ModelAndView("venda/input")
+                .addObject("venda", new Venda());
     }
 
-    redirectAttributes.addFlashAttribute("msgSucesso",
-            "Venda " + v.getId() + " cadastrada com sucesso");
-    return new ModelAndView("redirect:/gerenciamento/venda");
-  }
+    @RequestMapping("/{id}")
+    public ModelAndView abrirAlteracao(@PathVariable("id") Long idVenda) {
+        Venda v = vendaService.obter(idVenda);
+        return new ModelAndView("venda/input")
+                .addObject("venda", v);
+    }
+
+    @RequestMapping(value = "/{id}/remover", method = RequestMethod.POST)
+    public ModelAndView removerProduto(@PathVariable("id") Long idVenda) {
+        vendaService.remover(idVenda);
+        return new ModelAndView("redirect:/gerenciamento/venda");
+    }
+
+    @RequestMapping(value = "/salvar", method = RequestMethod.POST)
+    public ModelAndView salvar(
+            @ModelAttribute("venda") @Valid Venda v,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("venda/input");
+        }
+        boolean inclusao = (v.getId() == null);
+        v.setDtVenda(new Date());
+
+        if (inclusao) {
+            vendaService.incluir(v);
+        } else {
+            vendaService.alterar(v);
+        }
+
+        redirectAttributes.addFlashAttribute("msgSucesso",
+                "Venda " + v.getId() + " cadastrada com sucesso");
+        return new ModelAndView("redirect:/gerenciamento/venda");
+    }
 
 }
