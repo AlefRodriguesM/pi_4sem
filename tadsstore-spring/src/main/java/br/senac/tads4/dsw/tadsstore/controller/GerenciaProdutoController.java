0f644d37f65fd60f87d0aa82eaae687860ba0d9f
andrey.asantos1 <andrey.asantos1@CAS555078W0710.senacsp.edu.br>
2017-11-29 22:35:39 -0200

    Criando tela de estoque

--- tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/controller/GerenciaProdutoController.java
+++ tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/controller/GerenciaProdutoController.java
@@ -1,8 +1,12 @@
 package br.senac.tads4.dsw.tadsstore.controller;
 
+import br.senac.tads4.dsw.tadsstore.common.entity.Movimento;
 import java.util.Date;
 import br.senac.tads4.dsw.tadsstore.common.entity.Produto;
+import br.senac.tads4.dsw.tadsstore.common.service.MovimentoService;
 import br.senac.tads4.dsw.tadsstore.common.service.ProdutoService;
+import java.util.ArrayList;
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,39 +23,81 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/gerenciamento/produto")
 public class GerenciaProdutoController {
 
-  @Autowired
-  private ProdutoService produtoService;
-  
-  @RequestMapping
-  public ModelAndView abrirFormulario() {
-    return new ModelAndView("produto/input")
-	    .addObject("produto", new Produto());
-  }
-  
-  @RequestMapping("/{idProd}")
-  public ModelAndView editar(@PathVariable("idProd")Long idProduto) {
-    Produto p = produtoService.obter(idProduto);
+    @Autowired
+    private ProdutoService produtoService;
     
-    return new ModelAndView("produto/input").addObject("produto", p);
-  }
+    @Autowired
+    private MovimentoService movimentoService;
+    
+    private List<Produto> listaProdutos = new ArrayList();
 
-  @RequestMapping(value = "/incluir", method = RequestMethod.POST)
-  public ModelAndView incluir(@ModelAttribute("produto") @Valid Produto p, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-    p.setDtCadastro(new Date());
-    
-    if (p.getId() == null) {
-      produtoService.incluir(p);
-    } else {
-      produtoService.alterar(p);
+    @RequestMapping
+    public ModelAndView abrirFormulario() {
+        return new ModelAndView("produto/input")
+                .addObject("produto", new Produto());
     }
-    
-    return new ModelAndView("redirect:/produto/itens");
-  }
-  
-  @RequestMapping(path = "/excluir/{idProd}")
-  public ModelAndView excluir(@PathVariable("idProd") Long idProduto, RedirectAttributes redirectAttributes) {
-    produtoService.remover(idProduto);
-    
-    return new ModelAndView("redirect:/produto/itens");
-  }
+
+    @RequestMapping("/{idProd}")
+    public ModelAndView editar(@PathVariable("idProd") Long idProduto) {
+        Produto p = produtoService.obter(idProduto);
+
+        return new ModelAndView("produto/input").addObject("produto", p);
+    }
+
+    @RequestMapping(value = "/incluir", method = RequestMethod.POST)
+    public ModelAndView incluir(@ModelAttribute("produto") @Valid Produto p, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        p.setDtCadastro(new Date());
+
+        if (p.getId() == null) {
+            produtoService.incluir(p);
+        } else {
+            produtoService.alterar(p);
+        }
+
+        return new ModelAndView("redirect:/produto/itens");
+    }
+
+    @RequestMapping(path = "/excluir/{idProd}")
+    public ModelAndView excluir(@PathVariable("idProd") Long idProduto, RedirectAttributes redirectAttributes) {
+        produtoService.remover(idProduto);
+
+        return new ModelAndView("redirect:/produto/itens");
+    }
+
+    @RequestMapping("/estoque")
+    public ModelAndView listarProdEstoque() {
+        listaProdutos = produtoService.listar(0, 100);
+
+        return new ModelAndView("estoque/listaEstoque").addObject("listaProdutos", listaProdutos);
+    }
+
+    @RequestMapping(path = "/atualizarestoque/{idProd}")
+    public ModelAndView atualizarEstoque(
+            @PathVariable("idProd") Long idProduto,
+            @RequestParam("qtMovimento") int qtMovimento
+            ) {
+        
+        String tgMovimento = "";
+        int idPed = 0;
+        
+        if (qtMovimento < 0) {
+            tgMovimento = "S";
+        }else{
+            tgMovimento = "E";
+        }
+        
+        qtMovimento = Math.abs(qtMovimento);
+        
+        Movimento m = new Movimento();
+        
+        m.setDhMovimento(new Date());
+        m.setIdProduto(idProduto);
+        m.setQtMovimento(qtMovimento);
+        m.setTgMovimento(tgMovimento);
+        m.setIdPedido(Long.parseLong("0"));
+        
+        movimentoService.incluir(m);
+        
+        return new ModelAndView("redirect:/gerenciamento/produto/estoque");
+    }
 }
---
+++ tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/repository/MovimentoServiceJPAImpl1.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads4.dsw.tadsstore.repository;
+
+import br.senac.tads4.dsw.tadsstore.common.entity.Movimento;
+import br.senac.tads4.dsw.tadsstore.common.service.MovimentoService;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author andrey.asantos1
+ */
+@Repository
+public class MovimentoServiceJPAImpl1 implements MovimentoService{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Movimento> listar(int offset, int quantidade) {
+        Query query = entityManager.createQuery(
+                "SELECT DISTINCT m FROM Movimento m ");
+        return query.getResultList();
+    }
+    
+    @Override
+    public List<Movimento> listarProdutos(int offset, int quantidade, long idProduto) {
+        Query query = entityManager.createQuery(
+                "SELECT DISTINCT m FROM Movimento m "
+                + "WHERE m.idProduto = :idProduto").setParameter("idProduto", idProduto);
+        return query.getResultList();
+    }
+    
+    @Override
+    public List<Movimento> listarProdutosDoPedido(int offset, int quantidade, long idPedido) {
+        Query query = entityManager.createQuery(
+                "SELECT DISTINCT m FROM Movimento m "
+                + "WHERE m.idPedido = :idPedido").setParameter("idPedido", idPedido);
+        return query.getResultList();
+    }
+    
+    @Override
+    public Movimento obter(long idMovimento) {
+    Query query = entityManager.createQuery(
+            "SELECT DISTINCT m FROM Movimento m "
+            + "WHERE m.id = :idMovimento")
+            .setParameter("idMovimento", idMovimento);
+    return (Movimento) query.getSingleResult();
+  }
+
+    @Override
+    @Transactional
+    public void incluir(Movimento m) {
+        entityManager.persist(m);
+    }
+}
