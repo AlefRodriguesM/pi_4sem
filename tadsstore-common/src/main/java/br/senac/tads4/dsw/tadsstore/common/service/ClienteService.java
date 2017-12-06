871ae12b021e582ae65544cd36a719028c4a4270
vitor.lcosta <vitor.lcosta@CAS555079W0711.senacsp.edu.br>
2017-12-06 20:51:40 -0200

    Novo campo DS_PAPEL no cliente

--- tadsstore-common/src/main/java/br/senac/tads4/dsw/tadsstore/common/service/ClienteService.java
+++ tadsstore-common/src/main/java/br/senac/tads4/dsw/tadsstore/common/service/ClienteService.java
@@ -2,12 +2,15 @@ package br.senac.tads4.dsw.tadsstore.common.service;
 
 import br.senac.tads4.dsw.tadsstore.common.entity.Cliente;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public interface ClienteService extends Serializable {
     public List<Cliente> listar(int offset, int quantidade);
 
     public Cliente obter(long idCliente);
+
+    public ArrayList<Cliente> obterTodos();
 
     public void incluir(Cliente c);
 
