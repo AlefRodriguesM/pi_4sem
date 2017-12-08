c121b23c456cd8f61b8809a207ceff8407679ba8
Vitor <Vitor@DESKTOP-B67ATM4>
2017-12-07 22:37:25 -0200

    Voltando versão

--- tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/service/UsuarioService.java
+++ tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/service/UsuarioService.java
@@ -1,11 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package br.senac.tads4.dsw.tadsstore.service;
 
-import br.senac.tads4.dsw.tadsstore.common.entity.Cliente;
-import br.senac.tads4.dsw.tadsstore.common.service.ClienteService;
+import br.senac.tads4.dsw.tadsstore.config.SecurityConfig;
 import br.senac.tads4.dsw.tadsstore.model.Papel;
 import br.senac.tads4.dsw.tadsstore.model.UsuarioSistema;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,25 +19,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class UsuarioService implements UserDetailsService {
 
-    @Autowired
-    private static ClienteService clienteService;
+    private static final Map<String, UsuarioSistema> USUARIOS;
+
+    static {
+        USUARIOS = new LinkedHashMap<String, UsuarioSistema>();
+        USUARIOS.put("789", new UsuarioSistema("789",
+                SecurityConfig.passwordEncoder().encode("789"),
+                "Fulano da Silva",
+                Arrays.asList(new Papel("ROLE_FODINHA"))));
+        USUARIOS.put("456", new UsuarioSistema("456",
+                SecurityConfig.passwordEncoder().encode("456"),
+                "Ciclano de Souza",
+                Arrays.asList(new Papel("ROLE_FODINHA"), new Papel("ROLE_FODAO"))));
+
+        USUARIOS.put("123", new UsuarioSistema("123", SecurityConfig.passwordEncoder().encode("123"), "Beltrana das Cruzes",
+                Arrays.asList(new Papel("ROLE_FODINHA"), new Papel("ROLE_FODAO"), new Papel("ROLE_GOD"))));
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UsuarioSistema usu = new UsuarioSistema();
-
-        Cliente c = clienteService.obterByUsername(username);
-        List<Papel> p = (List<Papel>) new Papel();
-        Papel pa;
-        
-        p.add(pa = new Papel(c.getPapel()));
-        
-        usu.setId(c.getId());
-        usu.setNomeCompleto(c.getNome());
-        usu.setPapeis(p);
-        usu.setSenha(c.getSenha());
-        usu.setUsername(c.getEmail());
-        
-        return usu;
+        return USUARIOS.get(username);
     }
 }
